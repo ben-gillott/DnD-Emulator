d6de9c9674c779ca27d521fc048d677aeb69da7e
Benjamin Gillott <bg357@cornell.edu>
2019-12-31 16:33:24 -0500

    Click and Teleport

---
+++ Assets/clickmanager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class clickmanager : MonoBehaviour
+{
+    // public NormalTile normalTile;
+    // public Player selectedUnit;
+
+    private void OnMouseDown()
+    {
+        // left click - get info from selected tile
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello WOrld start");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            // Debug.Log("Hello WOrld");
+            // get mouse click's position in 2d plane
+            Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pz.z = 0;
+            Debug.Log("Mouse coords: " + pz);
+
+            // // convert mouse click's position to Grid position
+            GridLayout gridLayout = transform.parent.GetComponentInParent<GridLayout>();
+            Vector3 halfOff = new Vector3((float).5,(float).5,(float).5);
+            Vector3 cellPosition = gridLayout.WorldToCell(pz) + halfOff;
+        
+            GameObject.Find("PlayerCharacter").transform.position = cellPosition;
+            // // set selectedUnit to clicked location on grid
+            Debug.Log("Cell coords: " + cellPosition);
+        }
+    }
+}
